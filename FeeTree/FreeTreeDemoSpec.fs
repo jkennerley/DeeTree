557756a30bdd68fad12f9df57a85e0e885051f73
jk <john.kennerley@gmail.com>
2014-03-29 15:26:33 +0000

    basic f# decistion tree

--- FeeTree/FreeTreeDemoSpec.fs
+++ FeeTree/FreeTreeDemoSpec.fs
@@ -10,19 +10,19 @@ type Client ={
     CriminalRecord:bool
 }
 
-type QueryInfo = 
+type QueryInfo =
     {Title : string
      Check : Client->bool
      Positive : Decision
      Negative : Decision
     }
-and Decision = 
-    | Result of string 
+and Decision =
+    | Result of string
     | Query of QueryInfo
 
-module FeeTreeEvaluator =
-    let rec testClientTree(client, tree) = 
-        match tree with 
+module FreeTreeEvaluator =
+    let rec testClientTree(client, tree) =
+        match tree with
         | Result(message) -> printfn "offer loan %s" message
         | Query(qinfo) ->
             let result,case  =
@@ -31,35 +31,32 @@ module FeeTreeEvaluator =
             printfn " - %s ? %s" qinfo.Title result
             testClientTree(client, case)
 
-type ``Fee Tree Spec``() = 
+[<Trait("given Client Income=39000; CriminalRecord=true ; YearsInJob=0 ; UsesCreditCard=false", "")>]
+type ``Fee Tree Spec``() =
 
     [<Fact>]
-    member this.``f# decision treee basic``() = 
+    member this.``then should print NO in the console``() =
 
-        let rec tree = 
+        let rec tree =
             Query({ Title="More than 40K"
                     Check = (fun cl -> cl.Income > 40000 )
                     Positive = moreThan40;Negative = lessThan40})
-        and moreThan40 = 
+        and moreThan40 =
             Query({ Title="criminal record ?"
                     Check = (fun cl -> cl.CriminalRecord )
                     Positive = Result("NO");Negative = Result("YES")})
-        and lessThan40 = 
+        and lessThan40 =
             Query({ Title="Years in job ?"
                     Check = (fun cl -> cl.YearsInJob >1  )
                     Positive = Result("YES");Negative = usesCredit})
-        and usesCredit = 
+        and usesCredit =
             Query({ Title="uses credit ?"
                     Check = (fun cl -> cl.UsesCreditCard   )
                     Positive = Result("YES");Negative = Result("NO")})
 
         let john  = {Name="John";Income=39000;CriminalRecord=true;YearsInJob=0;UsesCreditCard=false}
 
-        FeeTreeEvaluator.testClientTree (john ,tree)
-
-        System.Console.WriteLine(".")
+        let ret = FreeTreeEvaluator.testClientTree (john ,tree)
 
         Assert.True |> ignore
         ()
-
-
